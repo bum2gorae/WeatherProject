2510877dc919af477d5c70ec22ca02bee4a30a3a
bum2gorae <ahddkfv@gmail.com>
2024-06-10 00:42:01 +0900

    Euclidean distance Check Require

---
+++ app/src/main/java/com/example/weatherproject/DustIF.kt
@@ -0,0 +1,23 @@
+//package com.example.weatherproject
+//
+//import android.app.Service
+//import retrofit2.Call
+//import retrofit2.http.GET
+//import retrofit2.http.Query
+//
+//interface DustIF {
+//
+//
+//    @GET("getMinuDustFrcstDspth?")
+//    fun getDust(
+//        @Query("searchDate") searchDate: String,   // 예보날짜
+//        @Query("returnType") returnType: String,
+//        @Query("serviceKey") serviceKey: String,
+//        @Query("numOfRows") numOfRows: Int,   // 한 페이지 경과 수
+//        @Query("pageNo") pageNo: Int,          // 페이지 번호
+//
+//
+//    ): Call<DustParsingClass>
+//
+//
+//}
